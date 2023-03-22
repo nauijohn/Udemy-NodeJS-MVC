5c7b0500f2015ddd6028202472eecd6f827856ca
Anthony Naui <85679013+nauijohn@users.noreply.github.com>
2023-03-22 13:06:18 +0800

    test

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,4 @@
 exports.get404 = (req, res, next) => {
   console.log('TEST');
-  res.status(400).render('404', { pageTitle: 'Page Not Found' });
+  res.status(400).render('404', { pageTitle: 'Page Not Found', path: '' });
 };
