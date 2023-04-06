580063095a4a7b1cecae13b0b90eb40e82f8983a
nauijohn <85679013+nauijohn@users.noreply.github.com>
2023-04-07 06:38:18 +0800

    Adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      pageTitle: product.title,
+      product: product,
+      path: '/products',
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('./shop/index', {
@@ -24,6 +35,19 @@ exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
     pageTitle: 'You Cart',
+  });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'You Orders',
   });
 };
 
